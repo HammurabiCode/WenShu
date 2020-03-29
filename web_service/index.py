5d6d0300eeca3c5378e2459a00100d96c1c41257
Hammurabi <breakawayroad@gmail.com>
2020-03-29 13:36:53 +0800

    update index.py

--- web_service/index.py
+++ web_service/index.py
@@ -7,7 +7,7 @@ bp = Blueprint('index', __name__, url_prefix='/index')
 
 @bp.route('')
 def index(): 
-    return 'Hello, World Six!'
+    return 'Hello, World Seven!'
     
     
 @bp.route('hi/<string:username>')
