9ee4f93b75bd4d240b27a6940ced4df9bbf41270
Hammurabi <breakawayroad@gmail.com>
2020-03-29 13:14:34 +0800

    add say_hi

--- web_service/index.py
+++ web_service/index.py
@@ -8,3 +8,8 @@ bp = Blueprint('index', __name__, url_prefix='/index')
 @bp.route('')
 def index(): 
     return 'Hello, World!'
+    
+    
+@bp.route('hi/<string:username>')
+def say_hi(username=''): 
+    return 'Hello, {}!'.format(username)
